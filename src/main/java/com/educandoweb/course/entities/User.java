c024beefdd495edb3f362368dc713912864833dd
Cristiano Pontes <pontescr@gmail.com>
2021-01-13 22:06:18 -0300

    Order, Instant, ISO 8601 parte 1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,11 +1,17 @@
 package com.educandoweb.course.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User {
 	
 	@Id
@@ -15,6 +21,9 @@ public class User {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -68,6 +77,10 @@ public class User {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -92,4 +105,5 @@ public class User {
 			return false;
 		return true;
 	}
+
 }
